e01144ee40d4ca76b5feb87f88917ca50f6486f1
GViveen <glenn.viveen@student.ru.nl>
2021-02-07 13:28:58 +0100

    Very minor memory improvement

--- dist_dict.py
+++ dist_dict.py
@@ -5,6 +5,6 @@ import itertools
 
 def build_dist_dict(coordinates):
     cities = {i:City(x, y) for i, (x, y) in enumerate(zip(coordinates[:,0], coordinates[:,1]))}
-    dist_dict = {p:cities[p[0]].calc_distance_to(cities[p[1]]) for p in itertools.product(cities, cities)}
+    dist_dict = {p:cities[p[0]].calc_distance_to(cities[p[1]]) for p in itertools.combinations(cities, 2)}
     
     return dist_dict
